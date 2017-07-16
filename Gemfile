bf6033dad1739615ba7931b5ed295f233a7b8847
mothermoose <melanie.ilseman@gmail.com>
2017-07-16 09:16:59 -0500

    Home controller updated

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'google-api-client'
 gem 'omniauth-google-oauth2'
 gem 'figaro'
 gem 'devise'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,15 @@
 class HomeController < ApplicationController
 
  def index
-   if current_student.admin
+    if current_student && current_student.admin
       redirect_to '/projects'
-    else  
+    elsif current_student
       redirect_to '/teams'
+    else
+      redirect_to '/students/sign_in'
     end 
-
-  end
+  end 
+  
 end
 
 
